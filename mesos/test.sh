5c5d52c4fe8622fca35b0ef7e3abf98c956531a1
Yuqian Yang <yyang@freewheel.tv>
2016-05-10 08:34:02 +0000

    add alignment

--- mesos/test.sh
+++ mesos/test.sh
@@ -8,8 +8,12 @@ ID=1
 for host in $HOSTS
 do
   init-docker-client $host
-  M=`docker exec -it mesos-master-node$ID mesos-resolve $MESOS_ZK|tail -n 1`
-  docker exec -it mesos-master-node$ID mesos-execute --master=$M --name="cluster-test" --command="echo cluster-test successfully > /tmp/hello.out"
+  MASTER=`docker exec -it mesos-master-node$ID mesos-resolve $MESOS_ZK|tail -n 1`
+  docker exec -it mesos-master-node$ID mesos-execute \
+    --master=$MASTER \
+    --name="cluster-test" \
+    --command="echo cluster-test successfully > /tmp/cluster-test.out"
+
   ID=$((ID + 1))
 done
 
